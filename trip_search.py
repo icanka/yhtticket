ae4a4347aa3fd74a017204a8e2f4b5d879bda068
izzetcan <izzetcankarakus@gmail.com>
2023-11-29 02:26:26 +0300

    changes

--- trip_search.py
+++ trip_search.py
@@ -7,21 +7,27 @@ from pprint import pprint
 SEATS_IDS = [13371893752, 13371893753, 16801693056, 16801693057]
 
 def get_empty_vagon_seats(vagon_json):
-
+    # business and economy class seat ids
+    empty_seat_ids = [13371893752, 13371893753, 16801693056, 16801693057]
     vagon_yerlesim = vagon_json['vagonHaritasiIcerikDVO']['vagonYerlesim']
     koltuk_durumlari = vagon_json['vagonHaritasiIcerikDVO']['koltukDurumlari']
-    pprint(len(koltuk_durumlari))
+    # efficient way to merge two lists of dictionaries based on a common key
     index_dict = {d['koltukNo'] : d for d in koltuk_durumlari if 'koltukNo' in d}
-    pprint(len(index_dict))
     merged_list = []
     for seat in vagon_yerlesim:
         seat_no = seat.get('koltukNo')
         if seat_no:
             merged_dict = {**seat, **index_dict.get(seat_no, {})}
             merged_list.append(merged_dict)
-    return merged_list
+            
+    # Take only the seats which has the key for nesneTanimId in empty_seat_ids
+    merged_list = [d for d in merged_list if d.get('nesneTanimId') in empty_seat_ids]
+    empty_seats = [d for d in merged_list if d.get('durum') == 0]
+    # yield items from empty_seats
+    for empty_seat in empty_seats:
+        yield empty_seat   
 
-
+SELECT_EMPTY_SEAT_ENDPOINT = "https://api-yebsp.tcddtasimacilik.gov.tr/koltuk/klSec"
 SEARCH_ENDPOINT = "https://api-yebsp.tcddtasimacilik.gov.tr/sefer/seferSorgula"
 VAGON_SEARCH_ENDPOINT = "https://api-yebsp.tcddtasimacilik.gov.tr/vagon/vagonBosYerSorgula"
 VAGON_HARITA_ENDPOINT = "https://api-yebsp.tcddtasimacilik.gov.tr/vagon/vagonHaritasindanYerSecimi"
@@ -63,17 +69,25 @@ vagon_req_body = {
     "inisIstId": None
 }
 
+
+seat_check = { 
+    "kanalKodu":"3",
+    "dil":0,
+    "seferId":None,
+    "seciliVagonSiraNo":None,
+    "koltukNo":None,
+}
 koltuk_sec_req_body = {
     "kanalKodu": "3",
     "dil": 0,
-    "seferId": 39260008468,
-    "vagonSiraNo": 8,
-    "koltukNo": "1b",
+    "seferId": None,
+    "vagonSiraNo": None,
+    "koltukNo": None,
     "cinsiyet": "E",
-    "binisIst": 234516104,
-    "inisIst": 234516259,
+    "binisIst": None,
+    "inisIst": None,
     "dakika": 10,
-    "huawei": 'false'
+    "huawei": False
 }
 
 request_body = {
@@ -189,9 +203,32 @@ for trip in trips:
                 response_json = json.loads(response.text)
 
 
+def get_empty_seats_trip(trip):
+    # clone trip object
+    trip_seats = trip.copy()
+    empty_seats = list()
+    vagon_req_body['seferBaslikId'] = trip['seferId']
+    vagon_req_body['binisIstId'] = trip['binisIstasyonId']
+    vagon_req_body['inisIstId'] = trip['inisIstasyonId']
+    # Send the request to the endpoint
+    response = requests.post(VAGON_SEARCH_ENDPOINT,
+                             headers=REQUEST_HEADER, data=json.dumps(vagon_req_body), timeout=10)
+    response_json = json.loads(response.text)
+    for vagon in response_json['vagonBosYerList']:
+        vagon_harita_req_body['vagonSiraNo'] = vagon['vagonSiraNo']
+        vagon_harita_req_body['seferBaslikId'] = vagon_req_body['seferBaslikId']
+        # Send the request to the endpoint
+        response = requests.post(VAGON_HARITA_ENDPOINT,
+                                 headers=REQUEST_HEADER, data=json.dumps(vagon_harita_req_body), timeout=10)
+        response_json = json.loads(response.text)
+        for empty_set in get_empty_vagon_seats(response_json):
+            empty_seats.append(empty_set)
+    trip_seats['empty_seats'] = empty_seats
+    return trip_seats
 # Get empty seats for each vagon in a trip
 for trip in trips:
     pprint(trip['binisTarih'])
+    pprint(trip['seferId'])
     vagon_req_body['seferBaslikId'] = trip['seferId']
     vagon_req_body['binisIstId'] = trip['binisIstasyonId']
     vagon_req_body['inisIstId'] = trip['inisIstasyonId']
@@ -200,16 +237,36 @@ for trip in trips:
                              headers=REQUEST_HEADER, data=json.dumps(vagon_req_body), timeout=10)
     vagons_json = json.loads(response.text)
     for vagon in vagons_json['vagonBosYerList']:
-        pprint(vagon)
-        vagon_harita_req_body['vagonSiraNo'] = vagon['vagonSiraNo']
-        vagon_harita_req_body['seferBaslikId'] = vagon_req_body['seferBaslikId']
-        # get vagon harita
-        response = requests.post(VAGON_HARITA_ENDPOINT,
-                                 headers=REQUEST_HEADER, data=json.dumps(vagon_harita_req_body), timeout=10)
-        vagon_harita_json = json.loads(response.text)
-        pprint(get_empty_vagon_seats(vagon_harita_json))
-        pprint(len(get_empty_vagon_seats(vagon_harita_json)))
-        exit(0)
+        if vagon['vagonSiraNo'] == 4:
+            pprint(f"vagon: {vagon}")
+            vagon_harita_req_body['vagonSiraNo'] = vagon['vagonSiraNo']
+            vagon_harita_req_body['seferBaslikId'] = vagon_req_body['seferBaslikId']
+            # get vagon harita
+            response = requests.post(VAGON_HARITA_ENDPOINT,
+                                    headers=REQUEST_HEADER, data=json.dumps(vagon_harita_req_body), timeout=10)
+            vagon_harita_json = json.loads(response.text)
+            empty_seats = get_empty_vagon_seats(vagon_harita_json)
+            
+            pprint(empty_seats)
+            pprint(len(get_empty_vagon_seats(vagon_harita_json)))
+            for empty_seat in empty_seats:
+                pprint(empty_seat)
+                koltuk_sec_req_body['seferId'] = trip['seferId']
+                koltuk_sec_req_body['vagonSiraNo'] = vagon['vagonSiraNo']
+                koltuk_sec_req_body['koltukNo'] = empty_seat['koltukNo']
+                koltuk_sec_req_body['binisIst'] = trip['binisIstasyonId']
+                koltuk_sec_req_body['inisIst'] = trip['inisIstasyonId']
+                    
+                pprint(json.dumps(koltuk_sec_req_body))
+                pprint(koltuk_sec_req_body)
+                # Send the request to the endpoint
+                response = requests.post(SELECT_EMPTY_SEAT_ENDPOINT,
+                                         headers=REQUEST_HEADER, data=json.dumps(koltuk_sec_req_body), timeout=10)
+                response_json = json.loads(response.text)
+                pprint(response_json)
+            exit(0)
+            #pprint(len(get_empty_vagon_seats(vagon_harita_json)))
+        
 
     # for yerlesim in vagon_yerlesim:
 
