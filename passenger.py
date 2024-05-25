21f7fce68cb2c966b7b5041814098824d07e7abd
izzetcan <izzetcankarakus@gmail.com>
2024-05-25 15:54:13 +0300

    changes
    Former-commit-id: 66361a144e592d510bd45dcfe8e734dfeb804b2e

--- passenger.py
+++ passenger.py
@@ -1,13 +1,16 @@
 """Passenger class to store passenger details."""
+
 import logging
 from typing import Optional
 import api_constants
 from dataclasses import dataclass, field
+
 logger = logging.getLogger(__name__)
 
 
 class Seat:
     """Seat class to store seat details."""
+
     BUSS: int = 17001
     ECO: int = 17002
     ANY = None
@@ -15,12 +18,15 @@ class Seat:
 
 class Tariff:
     """Tariff class to store tariff details."""
+
     TSK: int = 11750067704
     TAM: int = 1
+
 
 @dataclass(frozen=True)
 class Passenger:
     """Passenger class to store passenger details."""
+
     tckn: str
     name: str
     surname: str
@@ -33,10 +39,3 @@ class Passenger:
     credit_card_exp: Optional[str] = None
     tariff: Optional[int] = Tariff.TAM
     seat_type: Optional[int] = None
-
-
-# create a passenger object
-passenger = Passenger("18700774442", "izzet can", "karakus",
-                      "14/07/1994", "test@test.com", "05340771521", "E")
-
-print(passenger)
